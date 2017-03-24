b2f506872c69dd3e1f35e579e3640d2735e47e6d
fmakareev <fmakareev@gmail.com>
2017-03-24 13:09:13 +0300

    new style

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+jQuery("#input_range-1").slider({
+    min: 0,
+    max: 15,
+    values: [0, 15],
+    range: true
+});
+jQuery("#input_range-2").slider({
+    min: 0,
+    max: 11,
+    values: [0, 11],
+    range: true
+});
+jQuery("#input_range-3").slider({
+    min: 0,
+    max: 6,
+    values: [0, 6],
+    range: true
+});
