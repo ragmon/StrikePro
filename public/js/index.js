b4e10b4bc9020c877c67e182e974db5d84753cda
FMakareev <fmakareev@gmail.com>
2017-03-26 00:28:14 +0300

    addaptive menu

--- public/js/index.js
+++ public/js/index.js
@@ -16,3 +16,46 @@ jQuery("#input_range-3").slider({
     values: [0, 6],
     range: true
 });
+
+
+$(".productList__filtrBlock--close").on("click", function () {
+    var filtr = $(".productList__filtrBlock");
+    filtr.slideToggle();
+    $(".productList__filtrBlock--close").toggleClass("active");
+})
+
+$("#catalog__tabs").on("click", function (event) {
+    event.stopPropagation();
+})
+
+
+$(document).ready(function () {
+$('.accordion-tabs').children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+    $('.accordion-tabs').on('click', 'li > a', function (event) {
+        if (!$(this).hasClass('is-active')) {
+            event.preventDefault();
+            $('.accordion-tabs .is-open').removeClass('is-open').hide();
+            $(this).next().toggleClass('is-open').toggle();
+            $('.accordion-tabs').find('.is-active').removeClass('is-active');
+            $(this).addClass('is-active');
+        } else if ($(this).hasClass('is-active')){
+            event.preventDefault();
+            $(this).removeClass('is-active');
+            $(this).next().toggleClass('is-open').toggle();
+        } else {
+            event.preventDefault();
+        }
+    });
+});
+
+$(".headerNav__toggle").on("click", function(){
+    $(this).toggleClass("active")
+    if($(this).hasClass("active")){
+        $(".header__top").addClass("nav__active");
+        $("body").addClass("nav__active")
+    } else {
+        $(".header__top").removeClass("nav__active");
+        $("body").removeClass("nav__active");
+    }
+    
+})
