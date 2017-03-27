f5d79f770edf79b0accdb5092604ebad03fd3d53
fmakareev <fmakareev@gmail.com>
2017-03-27 12:18:40 +0300

    add popup js

--- src/js/index.js
+++ src/js/index.js
@@ -1,111 +1,131 @@
+// Slider page product
 $(document).ready(function () {
     var elemList = $(".colorTable__item");
     var slideContainer = $(".slider__wrap");
     var currentPosition = 0;
     var widthStep;
     var maxStep;
-    
-    function init(){
-        
-        for(var i = 0; i < elemList.length; i++){
+
+    function init() {
+
+        for (var i = 0; i < elemList.length; i++) {
             var imgLink = $(elemList[i]).find("img").attr("src");
             var imgAlt = $(elemList[i]).find("span").text();
-            var elem = '<li class="slider__item">' + '<a href=' + imgLink + ' class="slider__link">' + '<img src=' + imgLink + '  alt='+ imgAlt +'>' + '</a>' + '</li>';
+            var elem = '<li class="slider__item">' + '<a href=' + imgLink + ' class="slider__link">' + '<img src=' + imgLink + '  alt=' + imgAlt + '>' + '</a>' + '</li>';
             $(slideContainer).append(elem);
         }
-        
-        widthStep = $(slideContainer).find('li').outerWidth();
+
+        widthStep = $('.colorTable__gallery--slider').outerWidth() / 6;
         maxStep = (elemList.length * widthStep) - (widthStep * 6);
-        
+
         $(".colorTable__gallery--title__name").text($(".product__title h1").text());
     }
     init();
-    
-    $(".left").on("click",function(){
-        if(currentPosition == 0){
+
+    $(".left").on("click", function () {
+        if (currentPosition <= 0) {
             currentPosition = 0;
+            $(slideContainer).css({
+                "transform": "translateX(-" + currentPosition + "px)"
+            });
+            console.log(currentPosition);
             return;
         }
-        currentPosition -= widthStep; 
-        $(slideContainer).css({ "transform" : "translateX(-" + currentPosition + "px)"});  
+        console.log(currentPosition);
+        currentPosition -= widthStep;
+        $(slideContainer).css({
+            "transform": "translateX(-" + currentPosition + "px)"
+        });
     })
-    $(".right").on("click",function(){
-        if(currentPosition == maxStep){
+    $(".right").on("click", function () {
+        if (currentPosition == maxStep) {
             currentPosition = maxStep;
+            $(slideContainer).css({
+                "transform": "translateX(-" + currentPosition + "px)"
+            });
             return;
         }
-        currentPosition += widthStep; 
-        $(slideContainer).css({ "transform" : "translateX(-" + currentPosition + "px)"});   
+        currentPosition += widthStep;
+        $(slideContainer).css({
+            "transform": "translateX(-" + currentPosition + "px)"
+        });
     })
-    
-    $(".slider__item").on("click",function(event){
-        event.preventDefault();
-        
-        
-        $(".colorTable__gallery--img img").attr("src", $(this).find("img").attr("src"));
-        $(".colorTable__gallery--title__model").text($(this).find("img").attr("alt"));
-        
+
+    $(".slider__item").on("click", function (event) {
+            event.preventDefault();
+
+
+            $(".colorTable__gallery--img img").attr("src", $(this).find("img").attr("src"));
+            $(".colorTable__gallery--title__model").text($(this).find("img").attr("alt"));
+
+        })
+        // open popup
+    $(elemList).on("click", function (event) {
+        var windowSize = window.innerWidth;
+        if (windowSize >= 768) {
+            $(".colorTable__gallery--img img").attr("src", $(this).find("img").attr("src"));
+            $(".colorTable__gallery--title__model").text($(this).find("span").text());
+
+            $(".colorTable__gallery--wrapper").show();
+
+            $(".colorTable__gallery--popup").css({
+                "top": $(".colorTable__wrapper").offset().top + "px"
+            });
+            $("body").css({
+                "overflow": "hidden"
+            });
+            var scroll_el = $(".colorTable__wrapper");
+            if ($(scroll_el).length != 0) {
+                $('html, body').animate({
+                    scrollTop: $(scroll_el).offset().top - 80
+                }, 500);
+            }
+            return false;
+            if ($(this).index() >= (elemList.length - 5)) {
+                currentPosition = widthStep * ($(this).index() - 5);
+            } else {
+                currentPosition = widthStep * $(this).index();
+            }
+            $(slideContainer).css({
+                "transform": "translateX(-" + currentPosition + "px)"
+            });
+        }
     })
-    
-    $(elemList).on("click",function(event){
-        $(this).find("span").text();
-        $(".colorTable__gallery--img img").attr("src", $(this).find("img").attr("src"));
-        $(".colorTable__gallery--title__model").text($(this).find("span").text());
-        
-        currentPosition = widthStep * $( this ).index(); 
-        $(slideContainer).css({ "transform" : "translateX(-" + currentPosition + "px)"});  
-           
-        console.log();
-        
+
+    $(document).on("click", function (event) {
+        if ($(event.target).closest(".js__closePopup").length == 1) {
+            $(event.target).closest(".js__popup").hide();
+            $("body").css({
+                "overflow": "inherit"
+            });
+            return
+        } else if ($(event.target).closest(".colorTable__gallery--popup").length == 1) {
+            event.stopPropagation();
+            return
+        } else if ($(event.target).closest(".js__popup").length == 1) {
+            event.stopPropagation();
+            $(event.target).closest(".js__popup").hide();
+            $("body").css({
+                "overflow": "inherit"
+            });
+            $(".popUP__wrapper").detach();
+            return
+        }
     })
-    
-    
-    
+
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(".js__videoPopup").on("click", function (event) {
+    event.preventDefault();
+    var link = $(this).attr('href');
+    var popUp_content = '<iframe width="640" height="360" src="' + link + '" frameborder="0" allowfullscreen></iframe>';
+    var popUp = '<div class="popUP__wrapper js__popup active"><div class="popUP__video">' + popUp_content + '</div></div>';
+    $("body").css({
+        "overflow": "hidden"
+    });
+    $("body").append(popUp);
+})
 
 
 
