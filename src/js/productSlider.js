8c3d3c17758554e95acec4e638886452e1410159
FMakareev <fmakareev@gmail.com>
2017-07-14 21:56:49 +0300

    change gallery product desctop and mobile version

--- src/js/productSlider.js
+++ src/js/productSlider.js
@@ -6,43 +6,36 @@ $(document).ready(function () {
     var maxStep;
 
     function init() {
-
+        var slider__item_array = [];
+        $(slideContainer).empty();
         for (var i = 0; i < elemList.length; i++) {
-            var dataImg = "" + $(elemList[i]).attr("data-data-img");
-            var imgLink = $(elemList[i]).attr("src");
-            var imgBig = $(elemList[i]).attr("data-big-img");
+            var dataImg = JSON.parse($(elemList[i]).attr("data-data-img"));
 
+            var li = $('<li class="slider__item"></li>'),
+                link = $('<a href="" class="slider__link" data-data-img=""></a>'),
+                img = $('<img src="">');
 
-
-            var li = $('<li class="slider__item"></li>');
-            var link = $('<a href="" class="slider__link" data-data-img=""></a>');
-            var img = $('<img src="">');
-            $(link).attr({'data-data-img' : dataImg, 'href': imgBig});
-
-            if ( $(elemList[i]).attr("data-alt-img") ) {
-                $(link).attr({'data-alt-img' : $(elemList[i]).attr("data-alt-img")});
-            }
-
-            $(img).attr('src',imgLink);
+            $(img).attr('src', dataImg["0"].smallIMG);
+            $(img).attr('data-data-img', JSON.stringify(dataImg));
             $(link).append(img);
             $(li).append(link);
-            $(slideContainer).append(li);
+            slider__item_array.push(li);
         }
+        $(slideContainer).append(slider__item_array);
+        console.log(slider__item_array);
 
         widthStep = $('.colorTable__gallery--slider').outerWidth() / 6;
         maxStep = (elemList.length * widthStep) - (widthStep * 6);
 
         $(".colorTable__gallery--title__name").text($(".product__title h1").text());
 
-        $(".slider__link").on("click", function (event) {
+        $(".slider__link img").on("click", function (event) {
             event.preventDefault();
-            changeActiveImgVariation(this);
-            createCharacteristicsImg(this);
-            createAltImgList(this);
+            initVariation(this);
         });
 
         var $panzoom = $('.colorTable__gallery--img img').panzoom();
-        $panzoom.parent().on('mousewheel.focal', function( e ) {
+        $panzoom.parent().on('mousewheel.focal', function (e) {
             e.preventDefault();
             var delta = e.delta || e.originalEvent.wheelDelta;
             var zoomOut = delta ? delta < 0 : e.originalEvent.deltaY > 0;
@@ -52,46 +45,6 @@ $(document).ready(function () {
                 focal: e
             });
         });
-    }
-// Добавление характеристик товара над картинкой
-    function createCharacteristicsImg(elem) {
-        var container = $('.characteristics__list');
-        var imgData = jQuery.parseJSON($(elem).attr("data-data-img"));
-        $(container).empty();
-        for(var i = 0; i < imgData.length; i++){
-            var elem = '<li class="characteristics__item"><span class="characteristics__title">'+imgData[i].title+'</span><span class="characteristics__desc">'+imgData[i].data+'</span></li>';
-            $(container).append(elem);
-        }
-    }
-// добавление вариаций товара справа, если они есть
-    function createAltImgList(elem) {
-        var jsonListLink = $(elem).attr("data-alt-img");
-        var wrapperVariation = $('.colorTable__gallery--variation');
-        var containerVariation = $('ul.variation__list');
-        $(containerVariation).empty();
-
-        if(jsonListLink != undefined) {
-            var linkArray = jQuery.parseJSON(jsonListLink);
-            $(wrapperVariation).addClass('active');
-
-            for(var i = 0; i < linkArray.length; i++){
-                var li = $('<li class="variation__item"></li>');
-                var img = $('<img src="">');
-                $(img).attr('src', linkArray[i]);
-                $(li).append(img);
-                $(containerVariation).append(li);
-            }
-            $('li.variation__item').on('click', function () {
-                changeActiveImgVariation(this);
-            });
-            // -2665.67
-        } else {
-            $(wrapperVariation).removeClass('active');
-        }
-    }
-// изменение активного изображения
-    function changeActiveImgVariation(elem) {
-        $(".colorTable__gallery--img img").attr("src", $(elem).find("img").attr("src"));
     }
 
     $(".left").on("click", function () {
@@ -134,16 +87,67 @@ $(document).ready(function () {
     });
 
 
+    function initVariation(elem) {
+        var imgData = jQuery.parseJSON($(elem).attr("data-data-img"));
+        console.log(imgData);
+        var wrapperVariation = $('.colorTable__gallery--variation');
+        var containerVariation = $('ul.variation__list');
+        var liArray = [];
+        $(containerVariation).empty();
+        if (imgData.length > 1) {
+            for (var i = 0; i < imgData.length; i++) {
+                var li = $('<li class="variation__item"></li>');
+                var img = $('<img src="">');
+                $(img).attr('src', imgData[i].smallIMG);
+                $(img).attr('data-data-img', JSON.stringify(imgData[i]));
+                $(li).append(img);
+                liArray.push(li);
+            }
+            $(containerVariation).append(liArray);
+            $(wrapperVariation).addClass('active');
+            initCharacteristicsImg(imgData[0]);
+            initBigImg(imgData[0]);
+            $('li.variation__item img').on('click', function () {
+                initCharacteristicsImg(jQuery.parseJSON($(this).attr('data-data-img')));
+                initBigImg(jQuery.parseJSON($(this).attr('data-data-img')))
+            });
 
-        // open popup
+        } else {
+            $(wrapperVariation).removeClass('active');
+        }
+    }
+
+    function initBigImg(obj) {
+        $(".colorTable__gallery--img img").attr("src", obj.bigIMG);
+    }
+
+    function initCharacteristicsImg(obj) {
+        console.log(obj);
+        var container = $('.characteristics__list');
+        var imgData = obj.data;
+        console.log(imgData);
+        var charList = [];
+        $(container).empty();
+        for (var i = 0; i < imgData.length; i++) {
+            var char = '<li class="characteristics__item">' +
+                '<span class="characteristics__title">' + imgData[i].title + '</span>' +
+                '<span class="characteristics__desc">' + imgData[i].data + '</span>' +
+                '</li>';
+            charList.push(char);
+        }
+        $(container).append(charList);
+    }
+
+    // open popup
     $(elemList).on("click", function (event) {
         var windowSize = window.innerWidth;
         if (windowSize >= 768) {
-            $(".colorTable__gallery--img img").attr("src", $(this).attr("src"));
+            console.log(this);
+            // $(".colorTable__gallery--img img").attr("src", $(this).attr("src"));
 
-
-            createCharacteristicsImg(this);
-            createAltImgList(this);
+            initVariation(this);
+            // createCharacteristicsImg(this);
+            // createAltImgList(this);
 
             $(".colorTable__gallery--wrapper").show();
 
@@ -161,32 +165,20 @@ $(document).ready(function () {
                 "transform": "translateX(-" + currentPosition + "px)"
             });
         }
-    })
-
-
-
-
-
-
-
-
-
-
-
-
+    });
 
 
     $(".js-btn3D").on("click", function () {
         $(".product__stand .img_2d").hide();
         $("#image-reel").show()
-    })
+    });
 
     $(".product__sliderImg ul li a").on('click', function (event) {
         event.preventDefault();
         $("#image-reel").hide();
-        $(".product__stand .img_2d").show()
+        $(".product__stand .img_2d").show();
         $(".product__stand .img_2d").attr("src", $(this).attr("href"));
-    })
+    });
 
     $(window).resize(function () {
         var k = 500 / 304;
@@ -197,4 +189,88 @@ $(document).ready(function () {
         }
     });
 
+    function colorTable_item_slider() {
+        var colorTable__item = $('.colorTable__item'),
+            colorTable__item_length = colorTable__item.length;
+
+        for (var i = 0; i < colorTable__item_length; i++) {
+            var slide__array = [],
+                slider__data = JSON.parse($(colorTable__item[i]).find('img').attr('data-data-img')),
+                class__nav = ".custom-navigation-"+i+" a";
+
+            if (slider__data.length > 1) {
+                var slider__wrap = document.createElement('div'),
+                    slider__list = document.createElement('ul');
+                $(slider__wrap).addClass('slider-' + i);
+                $(slider__list).addClass('slides');
+                console.log(slider__data.length);
+                for (var a = 0; a < slider__data.length; a++) {
+                    var li = document.createElement('li'),
+                        img = document.createElement('img'),
+                        span = document.createElement('span');
+
+                    $(span).text(slider__data[a].data[0].data);
+                    $(img).attr('src', slider__data[a].bigIMG);
+                    $(li).append(img);
+                    $(li).append(span);
+                    slide__array.push(li)
+                }
+
+                $(slider__list).append(slide__array);
+                $(slider__wrap).append(slider__list);
+                $(slider__wrap).append('<div class="flex-direction-nav '+"custom-navigation-"+i+'">' +
+                    '<a href="#" class="flex-prev">' +
+                    '<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg_arrow" class="caret__arrow"></use></svg>'+
+                    '</a>' +
+                    '<a href="#" class="flex-next">' +
+                    '<svg><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg_arrow" class="caret__arrow"></use></svg>'+
+                    '</a>' +
+                    '</div>');
+                $(colorTable__item[i]).empty();
+                $(colorTable__item[i]).append(slider__wrap);
+                $(slider__wrap);
+
+                $(slider__wrap).flexslider({
+                    animation: "slide",
+                    slideshow: false,
+                    touch: true,
+                    directionNav: true,
+                    controlNav: false,
+                    customDirectionNav: $(class__nav)
+                });
+
+            }
+
+        }
+    }
+
+    var windowSize = window.innerWidth;
+    if (windowSize < 768) {
+
+        colorTable_item_slider();
+    }
+
 });
+
+
+// <div class="flexslider">
+//     <ul class="slides">
+//          <li>
+//              <img src="slide1.jpg" />
+//          </li>
+//          <li>
+//              <img src="slide2.jpg" />
+//          </li>
+//          <li>
+//              <img src="slide3.jpg" />
+//          </li>
+//          <li>
+//              <img src="slide4.jpg" />
+//          </li>
+//          </ul>
+//     </div>
+
+
+
+
+
